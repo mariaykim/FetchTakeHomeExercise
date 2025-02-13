d88afa258278e1a569b266a229cabd2c9fbe09be
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2025-02-13 13:23:30 -0500

    Initial Commit

---
+++ FetchTakeHomeExercise/FetchTakeHomeExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeExerciseApp.swift
+//  FetchTakeHomeExercise
+//
+//  Created by Maria Kim on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
